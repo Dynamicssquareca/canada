111f1880019c5f6280e087506bdcac4d5fe8c679
Sushil Behera <sbehera@mpgbis.com>
2024-04-04 15:15:58 +0530

    new update

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -12,7 +12,7 @@ const ContactForm = () => {
 
         try {
             // Send email using EmailJS
-            await emailjs.sendForm('service_fhump8w', 'template_vq0gu2g', event.target, 'K7wsWama116Jghyaq');
+            await emailjs.sendForm('service_fhump8ww', 'template_vq0gu2g', event.target, 'K7wsWama116Jghyaq');
 
             // Submit form data to Zoho CRM
             const response = await fetch(url, {
