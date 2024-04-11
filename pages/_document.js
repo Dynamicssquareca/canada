204fecbfc05d1053edac9af082f9c2cd95f3e379
Sushil Behera <sbehera@mpgbis.com>
2024-04-11 17:13:59 +0530

    update canda

--- pages/_document.js
+++ pages/_document.js
@@ -45,8 +45,8 @@ class MyDocument extends Document {
           <Main />
           <NextScript />
           <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-          {/* <script src="//code.jivosite.com/widget/ayximyoru5" async></script> */}
-          <script
+          <script src="//code.jivosite.com/widget/ayximyoru5" async></script>
+          {/* <script
             id="zsiqchat"
             async
             dangerouslySetInnerHTML={{
@@ -63,7 +63,7 @@ class MyDocument extends Document {
                 t.parentNode.insertBefore(s,t);
               `
             }}
-          />
+          /> */}
         </body>
       </Html>
     )
