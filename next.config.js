5205356eb0006c20b2622bb79135c1331184bcd2
Dynamicssquareca <dynamicssquareca@gmail.com>
2024-09-11 14:44:31 +0530

    update files

--- next.config.js
+++ next.config.js
@@ -13,18 +13,29 @@ const nextConfig = {
   async headers() {
     return [
       {
-        // Match all routes or specific routes
-        source: '/(.*)',
-
+        source: '/(.*\\.(js|css|png|jpg|jpeg|gif|svg|webp|avif))',  // Match all JS, CSS, and image files
         headers: [
           {
             key: 'Expires',
-            value: 'Wed, 21 Oct 2025 07:28:00 GMT', // Set the date for the Expires header
+            value: 'Wed, 21 Oct 2025 07:28:00 GMT', // Set far-future Expires header
+          },
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable', // One year caching, immutable content
+          },
+        ],
+      },
+      {
+        source: '/(.*)',  // Match all other routes (e.g., HTML files)
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate', // No caching for HTML files
           },
         ],
       },
     ];
-  }
+  },
 
 }
 
