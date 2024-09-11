610636f73a6fe6d26667cd43568606d664578ce4
Dynamicssquareca <dynamicssquareca@gmail.com>
2024-09-11 14:49:16 +0530

    update next js

--- next.config.js
+++ next.config.js
@@ -8,35 +8,7 @@ const nextConfig = {
    },
    env:{
     BACKEND_URL:'https://blognew.dynamicssquare.ca'
-  },
-
-  async headers() {
-    return [
-      {
-        source: '/(.*\\.(js|css|png|jpg|jpeg|gif|svg|webp|avif))',  // Match all JS, CSS, and image files
-        headers: [
-          {
-            key: 'Expires',
-            value: 'Wed, 21 Oct 2025 07:28:00 GMT', // Set far-future Expires header
-          },
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable', // One year caching, immutable content
-          },
-        ],
-      },
-      {
-        source: '/(.*)',  // Match all other routes (e.g., HTML files)
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=0, must-revalidate', // No caching for HTML files
-          },
-        ],
-      },
-    ];
-  },
-
+  }
 }
 
 
