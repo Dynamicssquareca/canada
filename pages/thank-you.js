a3e9378bf3fec579c0b43f6171283623822d13e3
Dynamicssquareca <dynamicssquareca@gmail.com>
2024-10-18 20:34:42 +0530

    update thankyou page

--- pages/thank-you.js
+++ pages/thank-you.js
@@ -21,25 +21,286 @@ const ThankYou = () => {
           href="https://www.dynamicssquare.ca/thank-you/"
         />
       </Head>
-      <section className="Solution-banner hero-1 custom-pd-4">
+      <section className="thnak-page">
         <div className="container">
-          <div className="row justify-content-center ">
-            <div className="col-lg-7 align-self-center text-center custom0004">
-              <h1>Thank You !</h1>
-              <h2>
-                Thanks for getting in touch - someone will contact you soon!
-              </h2>
-              <div className="text-center">
-                <div className="">
+          <div className="row justify-content-center">
+            <div className="col-lg-8 align-self-center">
+                <div className='iner-divv'>
+                <h1>Thank You !</h1>
+                <p>Hi there,</p>
+                <p>Thank you for contacting Dynamics Square! We’re excited to help you streamline your business with Microsoft Dynamics 365.</p>
+                <p>Our team will be in touch soon to discuss your needs. </p>
+                <p>Until then, stay updated with our latest updates and insights on <a href="https://www.linkedin.com/company/dynamics-square-canada/" target="_blank" ><b>LinkedIn</b></a>. </p>
+              <div className="text-left">
+                <div>
                   <a href="/" className="btn-get-started scrollto">
                     <span>Back to Home</span>
                   </a>
                 </div>
               </div>
+                </div>
             </div>
           </div>
         </div>
       </section>
+
+
+
+      <div className="footer-contact-s" style={{paddingTop:'60px'}}>
+        <div className="container">
+          <div className="row justify-content-center">
+          <div className="col-lg-4">
+              <div className="const-list-name">Ontario</div>
+              <div className="contact-list">
+                <div className="icons-left">
+                  <i className="bi bi-geo-alt"></i>
+                </div>
+                <div className="content-right">
+                  800 Steeles Ave. W. #B10155 Thornhill, ON. L4J 7L2
+                </div>
+              </div>
+              <div className="contact-list">
+                <div className="icons-left">
+                  <i className="bi bi-telephone"></i>
+                </div>
+                <div className="content-right">
+                  <a target="_self" rel="" href="tel:+12898070740">
+                  +1 289 807 0740
+                  </a>
+                </div>
+              </div>
+              <div className="contact-list">
+                <div className="icons-left">
+                  <i className="bi bi-envelope"></i>
+                </div>
+                <div className="content-right">info@dynamicssquare.ca</div>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <div className="const-list-name">British Columbia</div>
+              <div className="contact-list">
+                <div className="icons-left">
+                  <i className="bi bi-geo-alt"></i>
+                </div>
+                <div className="content-right">
+                  398-2416 Main St. Vancouver BC V5T 3E2
+                </div>
+              </div>
+              <div className="contact-list">
+                <div className="icons-left">
+                  <i className="bi bi-telephone"></i>
+                </div>
+                <div className="content-right">
+                  <a target="_self" rel="" href="tel:+17783815388">
+                  +1 778 381 5388
+                  </a>
+                </div>
+              </div>
+              <div className="contact-list">
+                <div className="icons-left">
+                  <i className="bi bi-envelope"></i>
+                </div>
+                <div className="content-right">info@dynamicssquare.ca</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <footer id="footer" className="footer">
+        <div className="footer-top">
+          <div className="container">
+            <div className="row gy-4">
+              <div className="col-lg-5 col-md-12 footer-info">
+                <a href="/" className="logo d-flex align-items-center">
+                  <img
+                    src="/img/dynamics_square_tm_logo_footer.svg"
+                    alt="Dynamics Square TM Logo"
+                    width="301"
+                    height="21"
+                    style={{ marginBottom: "20px" }}
+                  />
+                </a>
+                <p>Dynamics Square is Canada's leading Microsoft Gold Certified Partner
+                  that caters to the needs of orgnizations who can source
+                  next-gen Microsoft Business Apps from our highly skilled
+                  Microsoft consultants.</p>
+                <img
+                  src="/img/microsoft-partner-footer.png"
+                  alt="microsoft-partner"
+                  className="footer-img"
+                  width="210" height="74"
+                />
+                <img
+                  src="/img/Tech-For-Socil-Impact.svg"
+                  alt="Tech-For-Socil-Impact"
+                  className="footer-img"
+                  width="683" height="154"
+                />
+                <div className="footer-award">
+                  <img src="/img/award-1.png" alt='top micrsoft dynamics crm company' width="108" height="108" />
+                  <img src="/img/award-2.png" alt='top micrsoft dynamics crm company' width="108" height="108" />
+                  <img src="/img/award-3.png" alt='top micrsoft dynamics crm company' width="108" height="108" />
+                </div>
+              </div>
+              <div className="col-lg-2 col-12 footer-links">
+                <p>BUSINESS APPS</p>
+                <ul>
+                  <li>
+                    <i className="bi bi-chevron-right"></i>
+                    <a href="/solutions/dynamics-365-business-central/">
+                      Business Central
+                    </a>
+                  </li>
+                  <li>
+                    <i className="bi bi-chevron-right"></i>
+                    <a href="/solutions/microsoft-dynamics-365-finance/">
+                      Finance
+                    </a>
+                  </li>
+                  <li>
+                    <i className="bi bi-chevron-right"></i>
+                    <a href="/solutions/dynamics-365-supply-chain-management/">
+                      Supply Chain
+                    </a>
+                  </li>
+                  <li>
+                    <i className="bi bi-chevron-right"></i>
+                    <a href="/solutions/microsoft-dynamics-365-sales/">
+                      Sales
+                    </a>
+                  </li>
+                  <li>
+                    <i className="bi bi-chevron-right"></i>
+                    <a href="/solutions/microsoft-power-bi/">
+                      Power BI
+                    </a>
+                  </li>
+                  <li>
+                    <i className="bi bi-chevron-right"></i>
+                    <a href="/solutions/microsoft-azure/">Azure IoT</a>
+                  </li>
+                  <li>
+                    <i className="bi bi-chevron-right"></i>
+                    <a href="/solutions/microsoft-dynamics-erp/">
+                      ERP (NAV/GP/AX)
+                    </a>
+                  </li>
+                  <li>
+                    <i className="bi bi-chevron-right"></i>
+                    <a href="/solutions/microsoft-dynamics-crm/">
+                      CRM
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className="col-lg-2 col-12 footer-links">
+                <p>QUICK LINKS</p>
+                <ul>
+                  <li>
+                    <i className="bi bi-chevron-right"></i>
+                    <a href="/about-us/">About us</a>
+                  </li>
+                  <li>
+                    <i className="bi bi-chevron-right"></i>
+                    <a href="/why-us/">Why Us</a>
+                  </li>
+                  <li>
+                    <i className="bi bi-chevron-right"></i>
+                    <a href="/services/">Our Services</a>
+                  </li>
+                  <li>
+                    <i className="bi bi-chevron-right"></i>
+                    <a href="/partner/">Our Partners</a>
+                  </li>
+                  <li>
+                    <i className="bi bi-chevron-right"></i>
+                    <a href="/contact-us/">Contact Us</a>
+                  </li>
+                  <li>
+                    <i className="bi bi-chevron-right"></i>
+                    <a href="/blog/" rel="">
+                      Blogs
+                    </a>
+                  </li>
+                  <li>
+                    <i className="bi bi-chevron-right"></i>
+                    <a href="/privacy-policy/">Privacy policy</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="col-lg-3 col-md-12 text-md-start">
+                <p>CONNECT WITH US</p>
+
+                <div className="social-links mt-3">
+                  <a
+                    target="_blank"
+                    rel=""
+                    href="https://twitter.com/dsquare_ca"
+                    className="twitter"
+                  >
+                    <i className="bi bi-twitter"></i>
+                  </a>
+                  <a
+                    target="_blank"
+                    rel=""
+                    href="https://www.facebook.com/dynamicssquareca"
+                    className="facebook"
+                  >
+                    <i className="bi bi-facebook"></i>
+                  </a>
+                  <a
+                    target="_blank"
+                    rel=""
+                    href="https://www.youtube.com/c/DynamicsSquare"
+                    className="instagram"
+                  >
+                    <i className="bi bi-youtube"></i>
+                  </a>
+                  <a
+                    target="_blank"
+                    rel=""
+                    href="https://www.linkedin.com/company/dynamics-square-canada/"
+                    className="linkedin"
+                  >
+                    <i className="bi bi-linkedin"></i>
+                  </a>
+                </div>
+                <br />
+                {/* <div className="subbb">
+                    <p>Subscribe Newsletter</p>
+                    <span>
+                      Get Dynamics 365 Product Updates, Free Webinars, Blogs,
+                      Offers And Much More!
+                    </span>
+                    <FormFooterSubscriber />
+                  </div> */}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="copyright">
+            &copy; Copyright &nbsp;<span>2024 Dynamics Square.</span>
+            <div className="footer-extra-link">
+              <a href="/terms-of-use/">Terms of Use </a>
+              <a href="/cookie-policy/">| Cookie </a>
+              {/* <a href="/sitemap.xml">| Sitemap</a> */}
+            </div>
+          </div>
+        </div>
+      </footer>
+      <div className='call-bb-wr'>
+        <button className="fixed-button wobble" type="button">
+          <a href="tel:+12898070740" target="_self"><i className="bi bi-telephone-fill"></i></a>
+        </button>
+
+      </div>
+
+
+
+
+
     </>
   );
 };
