ff717f735bb78b5c4a872d86cbbd47a061ebdc69
Dynamicssquareca <dynamicssquareca@gmail.com>
2024-10-18 16:28:56 +0530

    update page

--- components/Footer.js
+++ components/Footer.js
@@ -600,7 +600,7 @@ const fetchCountryCodeByIP = () => {
         </div>
         <div className="container">
           <div className="copyright">
-            &copy; Copyright &nbsp;<span>2023 Dynamics Square.</span>
+            &copy; Copyright &nbsp;<span>2024 Dynamics Square.</span>
             <div className="footer-extra-link">
               <a href="/terms-of-use/" style={{color:'#ff6f7a'}}>Terms of Use </a>
               <a href="/cookie-policy/" style={{color:'#ff6f7a'}}>| Cookie </a>
--- pages/contact-us.js
+++ pages/contact-us.js
@@ -1062,7 +1062,7 @@ const ContactUs = () => {
         </div>
         <div className="container">
           <div className="copyright">
-            &copy; Copyright &nbsp;<span>2023 Dynamics Square.</span>
+            &copy; Copyright &nbsp;<span>2024 Dynamics Square.</span>
             <div className="footer-extra-link">
               <a href="/terms-of-use/">Terms of Use </a>
               <a href="/cookie-policy/">| Cookie </a>
